c298ff3c784f601e3f8a1442e994e6ac78812b6c
Matthias von Arx <vonarmat@students.zhaw.ch>
2022-04-17 05:25:18 +0200

    added a toString and getter to Node

--- app/src/main/java/Graphy/Graph/Node.java
+++ app/src/main/java/Graphy/Graph/Node.java
@@ -19,6 +19,14 @@ public class Node {
         return Objects.equals(value, node.value);
     }
 
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
     @Override
     public int hashCode() {
         return Objects.hash(value);
