65e9db00b9287eeacb836f2fde324e99d942a438
Matthias von Arx <vonarmat@students.zhaw.ch>
2022-04-17 05:04:12 +0200

    Added a graph implementation Added classes Edge, Graph and Node + Some ExampleInputFields

---
+++ app/src/main/java/Graphy/Graph/Graph.java
@@ -0,0 +1,46 @@
+package Graphy.Graph;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class Graph <T>{
+    boolean isUndirected;
+    boolean hasNegativeWeights = false;
+    boolean isWeighted = false; //if any of the weights is not equal to 0
+    private final Map<Node, Set<Edge>> adjacencySet = new HashMap<>();
+
+    public Graph(File file) throws FileNotFoundException, IOException {
+        Scanner scan = new Scanner(file);
+        if (scan.hasNextInt()){
+            isUndirected = scan.nextInt() != 0;
+        }
+        else {
+            throw new IOException("File has not the correct format");
+        }
+        while(scan.hasNextLine()){
+            String scannedLine = scan.nextLine();
+            String [] edgeArray = scannedLine.split(",");
+            if (edgeArray.length!=3) throw new IOException("File has not the correct format");
+            String start = edgeArray[0].strip();
+            String end = edgeArray[1].strip();
+            int weight = Integer.parseInt(edgeArray[2].strip());
+            if (weight<0) hasNegativeWeights = true;
+            if (weight!=0) isWeighted = true;
+            Node startNode = new Node(start);
+            Node endNode = new Node(end);
+            Edge edge = new Edge(startNode,endNode,weight);
+            if (!adjacencySet.containsKey(startNode)){
+                adjacencySet.put(startNode, new HashSet<>());
+            }
+            adjacencySet.get(startNode).add(edge);
+        }
+        scan.close();
+
+    }
+
+    public Map<Node, Set<Edge>> getAdjacencySet() {
+        return adjacencySet;
+    }
+}
---
+++ app/src/test/java/Graphy/Graph/NodeTest.java
@@ -0,0 +1,24 @@
+package Graphy.Graph;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void addEdge() {
+    }
+
+    @Test
+    void getAdjacencyList() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
