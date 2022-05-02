9bbcd7e82386d27ccb32c52acf21a5602371c4e6
Matthias von Arx <vonarmat@students.zhaw.ch>
2022-05-03 01:21:06 +0200

    corrected bfs

--- app/src/main/java/Graphy/Algorithms/BreadthFirstSearch.java
+++ app/src/main/java/Graphy/Algorithms/BreadthFirstSearch.java
@@ -1,13 +1,11 @@
 package Graphy.Algorithms;
 
+import Graphy.Graph.Edge;
 import Graphy.Graph.Graph;
 import Graphy.Graph.ValueHandler;
 import Graphy.Graph.Vertex;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 public class BreadthFirstSearch {
     private LinkedList<Vertex> adj[];
@@ -18,35 +16,38 @@ public class BreadthFirstSearch {
     void BFS(Graph graph, Vertex source, Vertex finalVertex) {
 
         // Mark all the vertices as not visited(By default set as false)
-        Map<Vertex, Boolean> visited = new HashMap<>();
-        for (Vertex vertex : handler.getGraph().keySet()){
-            visited.put(vertex, false);
+        Set<Vertex> visited = new HashSet<>();
+        Map<Vertex, Integer> distances = new HashMap<>();
+        Map<Vertex,Vertex> predecessor = new HashMap<>();
+
+        for (Vertex v : graph.getValueHandler().getGraph().keySet()){
+            distances.put(v,-1);
         }
 
         // Create a queue for BFS
-        LinkedList<Vertex> queue = new LinkedList<Vertex>();
-
+        Queue<Vertex> queue = new LinkedList<>();
+        distances.put(source,0);
         // Mark the current node as visited and enqueue it
-        visited.put(source, true);
+        visited.add(source);
         queue.add(source);
+        predecessor.put(source,null);
 
-        while (queue.size() != 0)
-        {
+        while (!queue.isEmpty()) {
             // Dequeue a vertex from queue and print it
-            source = queue.poll();
-            System.out.print(source+" ");
+            Vertex current  = queue.poll();
 
-            // Get all adjacent vertices of the dequeued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-
-            for (Vertex vertex : graph.getValueHandler().adjacentVertices(source)){
-                if (!visited.get(vertex))
-                {
-                    visited.put(vertex, true);
-                    queue.add(vertex);
+            //add all adjacent vertices to the queue
+            for (Vertex end : graph.getValueHandler().adjacentVertices(current)){
+                if (!visited.contains(end)){
+                    predecessor.put(end,current);
+                    queue.add(end);
+                    visited.add(end);
+                    distances.put(end,1+ distances.get(predecessor.get(end)));
+                    System.out.println("visited: " + end + "with distance: " + distances.get(end));
                 }
             }
+
+
         }
     }
 }
