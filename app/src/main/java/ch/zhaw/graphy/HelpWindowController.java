b652468fd36c4646792fd7bfded36f2c084cfebb
Tanja Aeberhardt <aebertan@students.zhaw.ch>
2022-05-06 10:51:59 +0200

    Set help informations to HelpWindowController #3

--- app/src/main/java/ch/zhaw/graphy/HelpWindowController.java
+++ app/src/main/java/ch/zhaw/graphy/HelpWindowController.java
@@ -14,31 +14,31 @@ public class HelpWindowController {
     private Stage stage;
     private boolean isPreWindow;
 
-    public HelpWindowController(boolean isPreWindow){
+    public HelpWindowController(boolean isPreWindow) {
         this.isPreWindow = isPreWindow;
-        try{
-			FXMLLoader helpLoader = new FXMLLoader(getClass().getResource("/HelpWindow.fxml"));
-			helpLoader.setController(this);
-			Stage helpStage = new Stage();
-			Pane rootNode = helpLoader.load();
-			Scene scene = new Scene(rootNode);
-			helpStage.setScene(scene);
-			helpStage.setMinWidth(280);
-			helpStage.setMinHeight(250);
-			this.stage = helpStage;
-			helpStage.show();
-		} catch(Exception e){
-		   e.printStackTrace();
-		}
+        try {
+            FXMLLoader helpLoader = new FXMLLoader(getClass().getResource("/HelpWindow.fxml"));
+            helpLoader.setController(this);
+            Stage helpStage = new Stage();
+            Pane rootNode = helpLoader.load();
+            Scene scene = new Scene(rootNode);
+            helpStage.setScene(scene);
+            helpStage.setMinWidth(280);
+            helpStage.setMinHeight(250);
+            this.stage = helpStage;
+            helpStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
-    public void initialize(){
-          setText();
-          textArea.setEditable(false);
+    public void initialize() {
+        setText();
+        textArea.setEditable(false);
     }
 
-    public Stage getStage(){
+    public Stage getStage() {
         return stage;
     }
 
@@ -54,16 +54,42 @@ public class HelpWindowController {
         stage.close();
     }
 
-    private void setText(){
-        if(isPreWindow){
+    private void setText() {
+        if (isPreWindow) {
             textArea.setText("You can choose between the two following options:" + System.lineSeparator()
-            + "Provided Graph: You have to provide a files" + System.lineSeparator()
-            + "Draw own Graph: You can draw your own graph in the main window. The help How it exactly works you wil");
-        } else{
-            textArea.setText("");
+                    + System.lineSeparator()
+                    + "Provided Graph:" + System.lineSeparator()
+                    + "You have to provide a graph file. Then the graph will be created using this file."
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "Draw own Graph:" + System.lineSeparator()
+                    + "You can draw your own graph in the following window."
+                    + System.lineSeparator() + System.lineSeparator() + System.lineSeparator()
+                    + "The help button in the following window has more informations about how the tool exactly works.");
+        } else {
+            textArea.setText("The buttons have the described functions:" + System.lineSeparator()
+                    + System.lineSeparator()
+                    + "Add edge:" + System.lineSeparator()
+                    + ""
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "Clear all:" + System.lineSeparator()
+                    + ""
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "Remove:" + System.lineSeparator()
+                    + ""
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "Change color:" + System.lineSeparator()
+                    + ""
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "Select alogrithm:" + System.lineSeparator()
+                    + "You can choose between the following algorithms:"
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "Print to CSV:" + System.lineSeparator()
+                    + "The displayed graph will be exported as a .csv file."
+                    + System.lineSeparator() + System.lineSeparator()
+                    + "Close:" + System.lineSeparator()
+                    + ""
+                    + System.lineSeparator() + System.lineSeparator());
         }
     }
 
 }
-
-
