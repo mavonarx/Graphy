d819d2bf7ae1d9117621df766aa42886714afcbe
grubelu1 <grubelu1@students.zhaw.ch>
2022-05-10 11:57:11 +0200

    Update module-info.java
    Added requiers for google.common to use a bidirectional Map BiMap

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module Graphy.app.main {
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.controls;
+    requires com.google.common;
     exports ch.zhaw.graphy;
     opens ch.zhaw.graphy;
 }
