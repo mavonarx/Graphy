3b9fde41e659ca7d851cf52d09331b9876a18a98
Matthias von Arx <vonarmat@students.zhaw.ch>
2022-05-11 13:10:56 +0200

    added javadoc do Edgeclass #46

--- app/src/main/java/ch/zhaw/graphy/Graph/Edge.java
+++ app/src/main/java/ch/zhaw/graphy/Graph/Edge.java
@@ -2,6 +2,10 @@ package ch.zhaw.graphy.Graph;
 
 import java.util.Objects;
 
+/**
+ * This class represents an edge in the graph. It has a start and an end vertex. And a weight.
+ * if no weight is given a default value of 0 is taken.
+ */
 public class Edge implements Comparable<Edge>{
     private final Vertex start;
     private final Vertex end;
@@ -9,6 +13,12 @@ public class Edge implements Comparable<Edge>{
     private static final int DEFAULT_WEIGHT = 0;
 
 
+    /**
+     * Constructor for an edge.
+     * @param start the start vertex
+     * @param end the end vertex
+     * @param weight the weight of the edge
+     */
     public Edge(Vertex start, Vertex end, int weight){
 
         if (end ==null || start ==null) throw new IllegalArgumentException("End- or startVertex cannot be null");
@@ -17,6 +27,11 @@ public class Edge implements Comparable<Edge>{
         this.weight = weight;
     }
 
+    /**
+     * constructor for edge with the default edge weight.
+     * @param start start vertex
+     * @param end end vertex
+     */
     public Edge(Vertex start, Vertex end){
 
         this( start, end, DEFAULT_WEIGHT);
@@ -35,11 +50,22 @@ public class Edge implements Comparable<Edge>{
         return start;
     }
 
+    /**
+     *
+     * @param other the object to be compared.
+     * @return >0 if this edge-weight is bigger than the other edge-weight, 0 if equal weights,
+     * <0 if this edge-weight is smaller than the other edge-weight
+     */
     @Override
     public int compareTo(Edge other) {
         return this.weight-other.weight;
     }
 
+    /**
+     * equal iff the positions are equal.
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -48,6 +74,10 @@ public class Edge implements Comparable<Edge>{
         return start.getPosition() == edge.start.getPosition() && end.getPosition() == edge.end.getPosition() && Objects.equals(end, edge.end);
     }
 
+    /**
+     * genereated hashcode
+     * @return
+     */
     @Override
     public int hashCode() {
         return Objects.hash(end, weight);
