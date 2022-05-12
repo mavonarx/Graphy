d0addab4ea1621fa693722856445875008668474
Matthias von Arx <vonarmat@students.zhaw.ch>
2022-05-12 14:42:27 +0200

    implemented curves #67

--- app/src/main/java/ch/zhaw/graphy/OwnBiMap.java
+++ app/src/main/java/ch/zhaw/graphy/OwnBiMap.java
@@ -7,12 +7,13 @@ import com.google.common.collect.HashBiMap;
 import javafx.scene.control.Label;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.QuadCurve;
 
 class OwnBiMap{
     private BiMap<Circle, Vertex> circleVertexList = HashBiMap.create();
     private BiMap<Circle, Label> circleLabelList = HashBiMap.create();
-    private BiMap<Line, Edge> lineEdgeBiMap = HashBiMap.create();
-    private BiMap<Line, Label> lineLabelList = HashBiMap.create();
+    private BiMap<QuadCurve, Edge> lineEdgeBiMap = HashBiMap.create();
+    private BiMap<QuadCurve, Label> lineLabelList = HashBiMap.create();
     void put(Circle circle, Label label, Vertex vertex){
         circleVertexList.put(circle, vertex);
         circleLabelList.put(circle, label);
@@ -24,10 +25,10 @@ class OwnBiMap{
     public BiMap<Circle, Label> getCircleLabelList() {
         return circleLabelList;
     }
-    public BiMap<Line, Edge> getLineEdgeBiMap() {
+    public BiMap<QuadCurve, Edge> getLineEdgeBiMap() {
         return lineEdgeBiMap;
     }
-    public BiMap<Line, Label> getLineLabelList() {
+    public BiMap<QuadCurve, Label> getLineLabelList() {
         return lineLabelList;
     }
 }
