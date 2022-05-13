bfcec5493802c0ce7e3ac3945e51559dca90e705
Matthias von Arx <vonarmat@students.zhaw.ch>
2022-05-13 09:49:47 +0200

    arrows now working again #67

--- app/src/main/java/ch/zhaw/graphy/EdgeGui.java
+++ app/src/main/java/ch/zhaw/graphy/EdgeGui.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EdgeGui {
+
+    private record ArrowInfo (int x, int y, int xEnd, int yEnd){}
     private static final Color stdLineColor = Color.LIGHTGRAY;
     private static final Color stdLineSelectedColor = Color.LIGHTBLUE;
     private static final Color stdTextColor = Color.BLACK;
@@ -32,7 +34,8 @@ public class EdgeGui {
     }
 
     private Point findCurve(int xStart, int xEnd, int yStart, int yEnd){
-        final int CURVE_ROUNDING = 30;
+        final double CURVE_ROUNDING = 30.0/200;
+        final double MAXFACTOR = 200;
         int x = xEnd-xStart;
         int y = yEnd-yStart;
         double twoNorm =  Math.sqrt(x*x + y*y);
@@ -43,18 +46,25 @@ public class EdgeGui {
         int halfy = yStart+(yEnd-yStart)/2;
 
 
-        x = (int )(halfx-CURVE_ROUNDING*yNorm);
-        y = (int)(halfy +CURVE_ROUNDING*xNorm);
+        double factor = Math.min(twoNorm, MAXFACTOR);
+
+        x = (int )(halfx-CURVE_ROUNDING*yNorm*factor);
+        y = (int)(halfy +CURVE_ROUNDING*xNorm*factor);
         return new Point(x,y);
     }
 
-    private Point findArrow(int xStart, int xEnd, int yStart, int yEnd, boolean up){
+    private ArrowInfo findArrow(int xStart, int xEnd, int yStart, int yEnd, boolean up){
         int vertexSize = VertexGui.VERTEX_SIZE;
         int x = xEnd-xStart;
         int y = yEnd-yStart;
         double twoNorm =  Math.sqrt(x*x + y*y);
         double xNorm = x/twoNorm;
         double yNorm = y/twoNorm;
+        final int ANGEL_GRAD = -12;
+        final double ANGEL_RAD = ANGEL_GRAD*Math.PI/180;
+        xNorm = xNorm*Math.cos(ANGEL_RAD)-yNorm*Math.sin(ANGEL_RAD);
+        yNorm = yNorm*Math.cos(ANGEL_RAD)+xNorm*Math.sin(ANGEL_RAD);
+
         if (up){
             x = (int )(xEnd -2*vertexSize*xNorm +vertexSize*yNorm);
             y = (int)(yEnd-2*vertexSize* yNorm -vertexSize*xNorm);
@@ -63,7 +73,7 @@ public class EdgeGui {
             x = (int )(xEnd -2*vertexSize*xNorm -vertexSize*yNorm);
             y = (int)(yEnd-2*vertexSize* yNorm +vertexSize*xNorm);
         }
-        return new Point(x,y);
+        return new ArrowInfo(x,y,(int)(xEnd-vertexSize*xNorm),(int)(yEnd-vertexSize*yNorm));
     }
 
     public EdgeGui(Edge edge, EdgeClickEvent event){
@@ -72,8 +82,8 @@ public class EdgeGui {
         int xEnd = edge.getEnd().getX();
         int yStart = edge.getStart().getY();
         int yEnd = edge.getEnd().getY();
-        Point pUp = findArrow(xStart,xEnd,yStart,yEnd,true);
-        Point pDown = findArrow(xStart,xEnd,yStart,yEnd,false);
+        ArrowInfo pUp = findArrow(xStart,xEnd,yStart,yEnd,true);
+        ArrowInfo pDown = findArrow(xStart,xEnd,yStart,yEnd,false);
 
         Point curve1 = findCurve(xStart,xEnd,yStart, yEnd);
         curve = new QuadCurve(xStart,yStart,curve1.x(),curve1.y(),xEnd,yEnd);
@@ -87,12 +97,10 @@ public class EdgeGui {
         clickArea.setStroke(Color.TRANSPARENT);
         clickArea.setOnMouseClicked(curveClick);
 
-        arrowline1 = new Line(xEnd,yEnd,pUp.x(),pUp.y());
-        arrowline2 = new Line(xEnd,yEnd,pDown.x(),pDown.y());
+        arrowline1 = new Line(pUp.xEnd,pUp.yEnd,pUp.x,pUp.y);
+        arrowline2 = new Line(pDown.xEnd,pDown.yEnd,pDown.x,pDown.y);
         arrowline1.setStrokeWidth(2);
         arrowline2.setStrokeWidth(2);
-        arrowline1.setFill(stdLineColor);
-        arrowline2.setFill(stdLineColor);
         arrowline1.setPickOnBounds(false);
         arrowline2.setPickOnBounds(false);
 
